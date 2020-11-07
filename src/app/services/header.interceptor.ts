be21a634ba6aae3d073c90077d18099beb1cbc37
Souvik Datta <datta357@gmail.com>
2020-11-07 19:52:04 +0530

    fixed submit redirection on enter

--- src/app/services/header.interceptor.ts
+++ src/app/services/header.interceptor.ts
@@ -9,13 +9,13 @@ export class HeaderInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
     
-     if (!req.body instanceof FormData){
+     if (!(req.body instanceof FormData)){
       req = req.clone({
         setHeaders:{
           'Content-Type': 'application/json',
         }
       });
-     }     
+     }   
       return next.handle(req);
   }
 }
