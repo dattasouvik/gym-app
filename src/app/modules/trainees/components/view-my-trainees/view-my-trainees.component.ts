67e1ad6dc204b3338bd476ad2f8935f47c192ecd
Your Name <you@example.com>
2020-11-10 14:01:26 +0530

    Change trainee design

--- src/app/modules/trainees/components/view-my-trainees/view-my-trainees.component.ts
+++ src/app/modules/trainees/components/view-my-trainees/view-my-trainees.component.ts
@@ -10,33 +10,18 @@ import { TraineesService } from '../../services/trainees.service';
   templateUrl: './view-my-trainees.component.html',
   styleUrls: ['./view-my-trainees.component.scss']
 })
-export class ViewMyTraineesComponent implements AfterViewInit, OnInit {
-  traineeData: Trainees[] = [];
+export class ViewMyTraineesComponent implements OnInit {
+  traineeData: Trainees[];
 
   constructor(public _getTrainees: TraineesService) { }
   ngOnInit(): void {
-    // this._getTrainees.getTrainees();
-    // this._getTrainees.profile$
-    //   .subscribe(arg => {
-    //     console.log(arg);
-    //     this.traineeData = arg;
-    //   });
 
     this._getTrainees.getTrainees1()
-      .subscribe(arg =>  this.traineeData = arg);
+      .subscribe(arg => { this.traineeData = arg.rows; console.log(arg) });
 
 
   }
 
-
-  displayedColumns: string[] = ['First Name', 'Last Name', 'Phone'];
-  dataSource = new MatTableDataSource<Trainees>(this.traineeData);
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
 
 }
 
